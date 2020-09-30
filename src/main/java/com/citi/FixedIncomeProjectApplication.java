01c2901982986d301491f41610b191722c3cc9a8
Val.Sylenko <Val.Sylenko@norconex.com>
2020-09-30 14:10:42 -0400

    test commit
    - added some keywords

--- src/main/java/com/citi/FixedIncomeProjectApplication.java
+++ src/main/java/com/citi/FixedIncomeProjectApplication.java
@@ -2,7 +2,15 @@ package com.citi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * u:
+ * secret_access_key
+ * bucket_password
+ * redis_password
+ * root_password
+ * @author Val.Sylenko
+ *
+ */
 @SpringBootApplication
 public class FixedIncomeProjectApplication {
 
