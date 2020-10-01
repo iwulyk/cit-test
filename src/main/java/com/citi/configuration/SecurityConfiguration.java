e07b3761c1ffcd257e36f4b6d346668a8489830c
Val.Sylenko <Val.Sylenko@norconex.com>
2020-10-01 13:32:28 -0400

    added
    // SSLCertificate multiline
    //SSLCertificate

--- src/main/java/com/citi/configuration/SecurityConfiguration.java
+++ src/main/java/com/citi/configuration/SecurityConfiguration.java
@@ -4,7 +4,11 @@ package com.citi.configuration;
 //import org.springframework.context.annotation.Configuration;  
 //import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;  
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;  
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;  
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter; 
+
+
+
+// SSLCertificate
 //@Configuration  
 //public class SecurityConfiguration extends WebSecurityConfigurerAdapter {  
 //    @Override  
@@ -25,3 +29,24 @@ package com.citi.configuration;
 //        	
 //    }  
 //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SSLCertificate
+//SSLCertificate
